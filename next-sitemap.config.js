82f679801d374e0d5096e21925e9da988f114151
Janis <63764361+LifeOfSins@users.noreply.github.com>
2025-10-03 14:54:41 +0000

    ay

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://degoriproformance.com", // replace with your domain
+  generateRobotsTxt: true, // generates robots.txt file
+  sitemapSize: 7000,
+};
