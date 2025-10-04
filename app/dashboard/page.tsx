e2e8cafd8d14a7adfd9174722afef244dea72ced
Janis <63764361+LifeOfSins@users.noreply.github.com>
2025-10-04 22:19:28 +0000

    hide sidebar for now

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,11 +21,11 @@ import FormTable from "@/components/form-table"
 export default function Page() {
   return (
     <SidebarProvider className="dark">
-      <AppSidebar />
+      {/* <AppSidebar /> */}
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1 text-primary" />
+            {/* <SidebarTrigger className="-ml-1 text-primary" /> */}
             <Separator
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
